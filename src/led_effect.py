800ee1d7613d36dd0cb2ab1ca6191a570ed21d66
Mitko Haralanov <voidtrance@gmail.com>
2023-04-20 10:18:44 -0700

    Allow layer definitions to be processed as templates
    This commit adds the ability to process the layer definitions as Jinja2
    templates just like any other Klipper macro. All of the normal objects
    available to macros are also available to layers.
    
    As an example, the following layer definition:
    
        [led_effect test1]
        autostart: false
        heater: extruder
        leds:
          neopixel:case_lights
        layers:
          {% set min_temp = printer.configfile.settings.extruder.min_extrude_temp %}
          heater {min_temp} 0 add (1,0,0)
    
    will result in the following layer being defined:
    
        heater 170.0 0 add (1,0,0)
    
    Layers are then processed as normal.
    
    It is also possible to have layers be re-evaluated when an effect is
    enabled. This has the benefit of using the current printer state.
    However, it will use CPU cycles to re-compute the layers.
    
    Signed-off-by: Mitko Haralanov <voidtrance@gmail.com>

--- src/led_effect.py
+++ src/led_effect.py
@@ -309,6 +309,7 @@ class ledEffect:
         self.config       = config
         self.printer      = config.get_printer()
         self.gcode        = self.printer.lookup_object('gcode')
+        self.gcode_macro  = self.printer.lookup_object('gcode_macro')
         self.handler      = self.printer.load_object(config, 'led_effect')
         self.frameRate    = 1.0 / config.getfloat('frame_rate', 
                                         default=24, minval=1, maxval=60)
@@ -347,8 +348,10 @@ class ledEffect:
         self.heater       = config.get('heater', None)
         self.analogPin    = config.get('analog_pin', None)
         self.stepper      = config.get('stepper', None)
+        self.recalculate  = config.get('recalculate', False)
         self.endstops     = [x.strip() for x in config.get('endstops','').split(',')]
-        self.configLayers = config.get('layers')
+        self.layerTempl   = self.gcode_macro.load_template(config, 'layers')
+        self.configLayers = []
         self.configLeds   = config.get('leds')
 
         self.nextEventTime = 0
@@ -400,7 +403,11 @@ class ledEffect:
                                  .lower() : c
                                    for c in self._layerBase.__subclasses__()
                                    if str(c).startswith("<class")}
+        self._generateLayers()
 
+    def _generateLayers(self):
+        self.layers = []
+        self.configLayers = self.layerTempl.render()
         for layer in [line for line \
             in self.configLayers.split('\n') if line.strip()]:
 
@@ -505,6 +512,8 @@ class ledEffect:
                 self.set_fade_time(parmFadeTime)
             self.set_enabled(False)
         else:
+            if self.recalculate:
+                self._generateLayers()
             if gcmd.get_int('REPLACE',0) >= 1:
                 for led in self.leds:
                     for effect in self.handler.effects:
