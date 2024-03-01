78bcca09aadc7e083f4988cc62e65a5af3e10de9
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-03-01 13:01:13 +0100

    fix(web_simulator): Relative base

--- packages/web_simulator/vite.config.ts
+++ packages/web_simulator/vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [svelte()],
   build: {
     rollupOptions: {
