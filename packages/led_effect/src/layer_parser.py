d16c9cffac71db5ec3c4f30262e508779fcbaa7a
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-03-02 11:46:31 +0100

    Add back in layer parser

---
+++ packages/led_effect/src/layer_parser.py
@@ -0,0 +1,44 @@
+import layer_parser_lark
+
+
+
+class TermTransformer(layer_parser_lark.Transformer):
+    NUMBER = float
+    RATE = float
+    CUTOFF = float
+    BLEND = str
+    LAYER_NAME = str
+    WORD = str
+    CNAME = str
+    
+    @layer_parser_lark.v_args(inline=True)
+    def ESCAPED_STRING(self, s):
+        return s[1:-1].replace('\\"', '"')
+
+
+layer_line_parser = layer_parser_lark.Lark_StandAlone()
+
+
+def parse(line):
+    tree = TermTransformer().transform(layer_line_parser.parse(line))
+
+    if tree.children[0].data == "legacy_line":
+        effect, rate, cutoff, blend, palette = tree.children[0].children
+        palette = [tuple([color for color in entry.children if color != None])
+                   for entry in palette.children]
+        return {
+            "effect": effect, "parameters": {"effectRate": rate, "effectCutoff": cutoff},
+            "blend": blend, "palette": palette
+        }
+    elif tree.children[0].data == "parameterized_line":
+        effect, parameters, blend, palette = tree.children[0].children
+
+        params = {k: v for k, v in [
+            (param.children[0], param.children[1]) for param in parameters.children]}
+        
+        palette = [tuple([color for color in entry.children if color != None])
+                   for entry in palette.children]
+        return {
+            "effect": effect, "blend": blend, "palette": palette, "parameters": params
+        }
+    return None
---
+++ packages/led_effect/tests/layer_parser_test.py
@@ -0,0 +1,42 @@
+import pytest
+import layer_parser
+
+legacy_format_data = [
+    ("breathing  .5 0 screen (0,.1,1), (0,1,.5), (0, 1,1), (0,.1,.5)", "breathing",
+     0.5, 0, "screen", [(0, 0.1, 1), (0, 1, 0.5), (0, 1, 1), (0, 0.1, 0.5)]),
+    ("breathing  .5 0 screen (0,.1,1,0.5), (0,1,.5), (0, 1,1), (0,.1,.5)", "breathing",
+     0.5, 0, "screen", [(0, 0.1, 1, 0.5), (0, 1, 0.5), (0, 1, 1), (0, 0.1, 0.5)]),
+    ("strobe         1  1.5   add        (1.0,  1.0, 1.0)",
+     "strobe", 1, 1.5, "add", [(1, 1, 1)]),
+    ("breathing      2  0     difference (0.95, 0.0, 0.0)",
+     "breathing", 2, 0, "difference", [(0.95, 0, 0)]),
+    ("static         1  0     top        (1.0,  0.0, 0.0)",
+     "static", 1, 0, "top", [(1, 0, 0)]),
+]
+
+parameterized_line_format_data = [
+    ("static()     top        (1.0,  0.0, 0.0)",
+     "static", {}, "top", [(1, 0, 0)]),
+    ("breathing(duration=.5) screen (0,.1,1), (0,1,.5), (0, 1,1), (0,.1,.5)", "breathing",
+     {"duration": 0.5}, "screen", [(0, 0.1, 1), (0, 1, 0.5), (0, 1, 1), (0, 0.1, 0.5)]),
+    ("breathing(duration=.5) screen (0,.1,1, 0.5), (0,1,.5), (0, 1,1), (0,.1,.5)", "breathing",
+     {"duration": 0.5}, "screen", [(0, 0.1, 1, 0.5), (0, 1, 0.5), (0, 1, 1), (0, 0.1, 0.5)]),
+    ("    strobe(frequency=1,decay=1.5)   add        (1.0,  1.0, 1.0)",
+     "strobe", {"frequency": 1, "decay": 1.5}, "add", [(1, 1, 1)]),
+    ("    strobe(frequency=1, decay=1.5)   add        (1.0,  1.0, 1.0)",
+     "strobe", {"frequency": 1, "decay": 1.5}, "add", [(1, 1, 1)]),
+    ("    heater(heater=heater_bed, decay=1.5)   add        (1.0,  1.0, 1.0)",
+     "heater", {"heater": "heater_bed", "decay": 1.5}, "add", [(1, 1, 1)]),
+]
+
+
+@pytest.mark.parametrize("line,effect,rate,cutoff,blend,palette", legacy_format_data)
+def test_parses_the_legacy_format(line, effect, rate, cutoff, blend, palette):
+    assert layer_parser.parse(line) == {
+        "effect": effect, "parameters": {"effectRate": rate, "effectCutoff": cutoff}, "blend": blend, "palette": palette}
+
+
+@pytest.mark.parametrize("line,effect,parameters,blend,palette", parameterized_line_format_data)
+def test_parses_the_parameterized_format(line, effect, parameters, blend, palette):
+    assert layer_parser.parse(line) == {
+        "effect": effect, "blend": blend, "palette": palette, "parameters": parameters}
