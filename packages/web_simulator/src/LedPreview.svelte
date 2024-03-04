3ddd7d63f98453e6bbb80b513581ae882b0db5ab
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-03-04 01:18:47 +0100

    feature(web_simulator): Add bootstrap

---
+++ packages/web_simulator/src/LedPreview.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let leds: number[][] = [];
+</script>
+
+
+<style>
+.preview-container {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    justify-items: center;
+    max-width: 600px;
+    margin: 0 auto;
+}
+</style>
+<div class="preview-container">
+    {#each leds as led, i}
+        <div
+            style={`background-color: rgb(${led.join(",")}); width: 50px; height: 50px; border-radius: 25px; margin: 5px; display: inline-block;`}
+        ></div>
+    {/each}
+</div>
