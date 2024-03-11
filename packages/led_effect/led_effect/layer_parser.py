18fa133f71c4d1cf7485030e178ef4a6244968cd
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-03-11 10:17:30 +0100

    chore(led_effect): Replace custom parsing with Tree Transformer

--- packages/led_effect/led_effect/layer_parser.py
+++ packages/led_effect/led_effect/layer_parser.py
@@ -9,46 +9,45 @@ class TermTransformer(layer_parser_lark.Transformer):
     LAYER_NAME = str
     WORD = str
     CNAME = str
+    palette = list
 
     @layer_parser_lark.v_args(inline=True)
     def ESCAPED_STRING(self, s):
         return s[1:-1].replace('\\"', '"')
 
+    def float_triplet(self, triplet):
+        return tuple(triplet)
 
-layer_line_parser = layer_parser_lark.Lark_StandAlone()
+    def palette_entry(self, children):
+        return children[0]
 
+    def hex(self, hex):
+        return tuple([int(hexVal,16)/255.0 for hexVal in hex])
 
-def parse_palette(palette):
-    return [parse_color(color)
-            for entry in palette.children
-            for color in entry.children if entry is not None]
-
-
-def parse_color(tripletOrHex):
-    if tripletOrHex.data == "float_triplet":
-        return tuple(tripletOrHex.children)
-    return tuple([int(hexVal,16)/255.0 for hexVal in tripletOrHex.children])
-    
-
-
-def parse(line):
-    tree = TermTransformer().transform(layer_line_parser.parse(line))
-
-    if tree.children[0].data == "legacy_line":
-        effect, rate, cutoff, blend, palette = tree.children[0].children
-        palette = parse_palette(palette)
+    def legacy_line(self, children):
+        effect, rate, cutoff, blend, palette = children
         return {
             "effect": effect, "parameters": {"effectRate": rate, "effectCutoff": cutoff},
             "blend": blend, "palette": palette
         }
-    elif tree.children[0].data == "parameterized_line":
-        effect, parameters, blend, palette = tree.children[0].children
+
+    def parameterized_line(self, children):
+        effect, parameters, blend, palette = children
 
         params = {k: v for k, v in [
             (param.children[0], param.children[1]) for param in parameters.children]}
 
-        palette = parse_palette(palette)
         return {
             "effect": effect, "blend": blend, "palette": palette, "parameters": params
         }
-    return None
+
+
+layer_line_parser = layer_parser_lark.Lark_StandAlone()
+
+
+
+
+def parse(line):
+    tree = TermTransformer().transform(layer_line_parser.parse(line))
+    return tree.children[0]
+    
