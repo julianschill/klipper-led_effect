b304a74f1a0640e6cdaff390c2fcfb75f8fe1f54
Julian Schill <j.schill@web.de>
2024-05-20 14:32:07 +0200

    hotfix for loading gcode_macro object

--- src/led_effect.py
+++ src/led_effect.py
@@ -307,7 +307,7 @@ class ledEffect:
         self.config       = config
         self.printer      = config.get_printer()
         self.gcode        = self.printer.lookup_object('gcode')
-        self.gcode_macro  = self.printer.lookup_object('gcode_macro')
+        self.gcode_macro  = self.printer.load_object(config, 'gcode_macro')
         self.handler      = self.printer.load_object(config, 'led_effect')
         self.frameRate    = 1.0 / config.getfloat('frame_rate', 
                                         default=24, minval=1, maxval=60)
