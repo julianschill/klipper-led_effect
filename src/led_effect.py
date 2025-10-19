3bc7ba2d02beec39f84ffb5b1cd37ef0a71f71aa
Julian Schill <j.schill@web.de>
2025-10-19 22:26:16 +0200

    Fix LED update to work with latest Klipper changes

--- src/led_effect.py
+++ src/led_effect.py
@@ -107,8 +107,9 @@ class ledFrameHandler:
             if not effect.runOnShutown:
                 for chain in self.ledChains:
                     chain.led_helper.set_color(None, (0.0, 0.0, 0.0, 0.0))
-                    chain.led_helper.update_func(chain.led_helper.led_state, None)
-
+                    chain.led_helper.need_transmit = True
+                    chain.led_helper._check_transmit()
+                    
         pass
     
     def _handle_homing_move_begin(self, hmove):
@@ -235,7 +236,8 @@ class ledFrameHandler:
             if hasattr(chain,"prev_data"):
                 chain.prev_data = None # workaround to force update of dotstars
             if not self.shutdown: 
-                chain.led_helper.update_func(chain.led_helper.led_state, None)
+                chain.led_helper.need_transmit = True
+                chain.led_helper._check_transmit()
         if self.effects:
             next_eventtime=min(self.effects, key=lambda x: x.nextEventTime)\
                             .nextEventTime
